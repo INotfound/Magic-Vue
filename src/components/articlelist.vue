77f9fe8d07b099dea3267a96a20c91bb7a056420
INotfound <INotfound@github.com>
2020-12-24 18:59:10 +0800

    ArticleList去Element元素

--- src/components/articlelist.vue
+++ src/components/articlelist.vue
@@ -1,38 +1,35 @@
 <template>
     <div>
-        <router-link
-            tag="div"
-            class="hover mt-10"
-            :to="{path:'article/details',query:{id:item.id}}"
-            v-for="(item,i) in articleInfo"
-            :key="i">
-                <el-card shadow="hover">
+        <div style="margin-top: 10px" height="150" v-for="(item,i) in articleInfo"  :key="i">
+                <div class="card">
                     <div class="d-lf">
                         <img
-                            :src="item.imgUrl"
                             alt
+                            :src="item.imgUrl"
+                            height="155" width="155"
+                            style="margin:10px 10px 10px 10px;border-radius: 3px;"
                         />
-                        <div class="m-20">
-                            <div class="article-title" >
+                        <div class="m-20" >
+                            <router-link tag="div"  class="article-title" :to="{path:'article/details',query:{id:item.id}}" >
                                 <span style="overflow-wrap: break-word;">{{item.title}}</span>
-                            </div>
+                            </router-link>
                             <div class="mt-10">
                                 <span style="overflow-wrap: break-word;">{{item.summary}}</span>
                             </div>
                             <div class="time mt-40 d-f">
                                 <div>
-                                    <span class="el-icon-time"></span>
+                                    <!-- <span class="el-icon-time"></span> -->
                                     <span class="ml-5">{{item.createTime}}</span>
                                 </div>
                                 <div class="ml-20">
-                                    <span class="el-icon-view"></span>
+                                    <!-- <span class="el-icon-view"></span> -->
                                     <span class="ml-5">{{item.browse}} 已阅读</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </el-card>
-        </router-link>
+                </div>
+        </div>
     </div>
 </template>
 
@@ -48,9 +45,20 @@ export default {
 .time {
   color: #748594;
 }
+.card{
+    transition: all 0.5s;
+    border-radius: 3px;
+    border: 1px rgb(0, 0, 0);
+    background-color:#FFFFFF;
+    box-shadow:0px 0px 4px rgba(0, 0, 0, 0.1);
+}
+.card:hover{
+    box-shadow:0px 0px 20px rgba(0, 0, 0, 0.15);
+}
 .article-title {
-  font-size: 20px;
   color: #555;
+  font-size: 20px;
+  cursor: pointer;
   font-weight: bold;
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -27,12 +27,12 @@ export default {
     data() {
         return {
             articleListInfo:[
-                {id:1,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"},
-                {id:2,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"},
-                {id:3,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"},
-                {id:4,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"},
-                {id:5,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"},
-                {id:6,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"http://www.zbboke.com/uploads/allimg/180626/1-1P626192T9222-lp.png"}
+                {id:1,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"},
+                {id:2,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"},
+                {id:3,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"},
+                {id:4,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"},
+                {id:5,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"},
+                {id:6,title:"小白君真帅",summary:"小白君是真的帅",createTime:"2020-3-31",browse:2400,imgUrl:"https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00358-4157.jpg"}
             ],
         }
     },
